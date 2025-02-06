207441cdd4e189d6f652a5bd2e4b99df1eb7946d
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2025-02-07 01:26:52 +0530

    Update reader.js

--- js/reader.js
+++ js/reader.js
@@ -126,3 +126,20 @@ function addComment() {
 
     // In a real application, you would save this to a database
 }
+
+// nvbar funvtionality start
+const nav = document.querySelector(".nav"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+// nvbar funvtionality end
+
+
